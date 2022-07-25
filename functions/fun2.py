05f63102840012d854926e305305dfa3951ab653
zaidkamil <xaidmetamorphos@gmail.com>
2022-07-25 17:07:55 +0530

    function #1

---
+++ functions/fun2.py
@@ -0,0 +1,10 @@
+
+def vowel_counter(sentence):
+    result = {}                 # create a dictionary
+    for vowel in "aeiou":       # loop through the vowels
+        result[vowel] = sentence.lower().count(vowel)
+    return result
+
+if __name__ == "__main__":
+    msg = "This is a test"
+    print(vowel_counter(msg))
---
+++ functions/fun4.py
@@ -0,0 +1,4 @@
+# create a function for adding value
+# create a function for subtracting value
+# create a function for multiplying value
+# create a function for dividing value
